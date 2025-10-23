a3e30e4f5548336fe288232b537ac7b82061e6c1
hidra <yhidraxz@gmail.com>
2025-10-23 19:55:07 -0300

    created full BioMatch dr lp

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./page/HomePage.jsx";
+import React from "react";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Garantia from "./components/Garantia";
+import Testimonials from "./components/Testimonials";
+import Pricing from "./components/Pricing";
+import FAQ from "./components/FAQ";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div data-theme="dark">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="min-h-screen flex flex-col bg-black">
+      <main className="flex-grow">
+        <Hero />
+        <div className="bg-black">
+          <Features />
+          <Pricing />
+          <Garantia />
+          <Testimonials />
+
+          <FAQ />
+          <CTA />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
-
-export default App;
